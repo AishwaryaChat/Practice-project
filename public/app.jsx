923ce2c493cbe4f20956f0912029832b51519c90
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-08 11:26:39 +0530

    added nested components

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,38 @@
+const GreeterMessage = React.createClass ({
+  render: function () {
+    const name = this.props.name
+    const message = this.props.message
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+    )
+  }
+})
+
+const GreeterForm = React.createClass ({
+  onFormSubmit: function (e) {
+    e.preventDefault() // this prevents the form from submitting and causing a page refresh or a browser reload
+    const name = this.refs.name.value
+
+    if (name.length > 0) {
+      this.refs.name = ''
+      this.props.onNewName(name)
+    }
+  },
+  render: function () {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input type='text' ref='name' />
+          <button>Set Name</button>
+        </form>
+      </div>
+    )
+  }
+})
+
 const Greeter = React.createClass({
   // this method is byDefault provided by react and called whenver ReactDOM.render is called
   getDefaultProps: function () { // wil set dafault props for the class
@@ -14,23 +49,13 @@ const Greeter = React.createClass({
     }
   },
 
-  // user defined method
-  OnButtonClick: function (e) {
-    e.preventDefault() // this prevents the form from submitting and causing a page refresh or a re-browser reload
-    let nameRefs = this.refs.name
-    const name = nameRefs.value
-    nameRefs.value = '' // making the input text value null after state change
-
-    // checking if the input text value passed contains something or not
-    if (typeof name === 'string' && name.length > 0) {
-      // sets the new state of the component
-      this.setState(
-        {
-          name: name
-        }
-      )
-    }
+  // user defined method, passing the state to children component
+  handleNewName: function (name) {
+    this.setState({
+      name: name
+    })
   },
+
   // this method is also byDefault provided by react and called whenver ReactDOM.render is called
   render: function () {
     const name = this.state.name // accessing properties passed to the class
@@ -38,13 +63,10 @@ const Greeter = React.createClass({
     /* onSubmit expects a function to be passed to it */
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message + '!!!!'}</p>
 
-        <form onSubmit={this.OnButtonClick}>
-          <input type='text' ref='name' />
-          <button>Set Name</button>
-        </form>
+      <GreeterMessage name={name} message={message} />
+
+        <GreeterForm onNewName={this.handleNewName} />
       </div>
     )
   }
@@ -53,6 +75,6 @@ const Greeter = React.createClass({
 const firstName = 'Aishwarya' // passing a variable in props
 
 ReactDOM.render(
-  <Greeter name={firstName} message='Message from props' />,
+  <Greeter name={firstName} />,
   document.getElementById('app')
 )
