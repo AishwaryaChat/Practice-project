8f2d564b5ba7e9ddc483fed85b42744a8e052e5e
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-08 13:02:44 +0530

    make the childrn components dynamic

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,5 @@
-const GreeterMessage = React.createClass ({
+// Presentational component
+const GreeterMessage = React.createClass({
   render: function () {
     const name = this.props.name
     const message = this.props.message
@@ -11,28 +12,46 @@ const GreeterMessage = React.createClass ({
   }
 })
 
-const GreeterForm = React.createClass ({
+// Presentational component
+const GreeterForm = React.createClass({
   onFormSubmit: function (e) {
     e.preventDefault() // this prevents the form from submitting and causing a page refresh or a browser reload
+    const updates = {}
     const name = this.refs.name.value
+    const message = this.refs.message.value
 
     if (name.length > 0) {
-      this.refs.name = ''
-      this.props.onNewName(name)
+      this.refs.name.value = ''
+      updates.name = name
     }
+
+    if (message.length > 0) {
+      this.refs.message.value = ''
+      updates.message = message
+    }
+
+    this.props.onNewData(updates)
   },
   render: function () {
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type='text' ref='name' />
-          <button>Set Name</button>
+          <div>
+            <input type='text' ref='name' placeholder='Enter Name' />
+          </div>
+          <div>
+            <textArea rows='2' ref='message' placeholder='Enter Message' />
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
         </form>
       </div>
     )
   }
 })
 
+// Container component
 const Greeter = React.createClass({
   // this method is byDefault provided by react and called whenver ReactDOM.render is called
   getDefaultProps: function () { // wil set dafault props for the class
@@ -45,28 +64,26 @@ const Greeter = React.createClass({
   // default method of react to get initial state of component
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     }
   },
 
   // user defined method, passing the state to children component
-  handleNewName: function (name) {
-    this.setState({
-      name: name
-    })
+  handleNewData: function (updates) {
+    this.setState(updates)
   },
 
   // this method is also byDefault provided by react and called whenver ReactDOM.render is called
   render: function () {
     const name = this.state.name // accessing properties passed to the class
-    const message = this.props.message
+    const message = this.state.message
     /* onSubmit expects a function to be passed to it */
     return (
       <div>
+        <GreeterMessage name={name} message={message} />
 
-      <GreeterMessage name={name} message={message} />
-
-        <GreeterForm onNewName={this.handleNewName} />
+        <GreeterForm onNewData={this.handleNewData} />
       </div>
     )
   }
